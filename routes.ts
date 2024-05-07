1a26218c5ae9ce7212af15d6b71140a58fbc7c7d
Andrey Chorniy <creatuseandr@icloud.com>
2024-05-07 21:41:23 +0300

    add new url to auth routes

--- routes.ts
+++ routes.ts
@@ -1,6 +1,10 @@
 export const publicRoutes = ['/'];
 
-export const authRoutes = ['/auth/login', '/auth/register'];
+export const authRoutes = [
+  '/auth/login',
+  '/auth/register',
+  '/auth/error',
+];
 
 export const apiAuthPrefix = '/api/auth';
 
