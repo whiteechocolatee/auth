afd57d20833d43c634d8609e230bf18c831bd476
Andrey Chorniy <creatuseandr@icloud.com>
2024-05-13 19:06:46 +0300

    Implement signIn function with email verification check

--- auth.ts
+++ auth.ts
@@ -45,6 +45,21 @@ export const {
     },
   },
   callbacks: {
+    async signIn({ user, account }) {
+      // allow OAuth without email verification
+      if (account?.provider !== 'credentials') {
+        return true;
+      }
+
+      const existingUser = await getUserById(user.id);
+
+      // Prevent signIn without email verification
+      if (!existingUser?.emailVerified) return false;
+
+      // TODO: Add 2FA check
+
+      return true;
+    },
     async session({ session, token }) {
       console.log({ token });
 
